7f3bcefc2425430b7f50bad9d121ec9b887f44e5
Jolanda <ad11jnav@uwcad.it>
2019-05-24 18:21:03 +0800

    wip reworking to not depend on elm-css

--- src/Styles.elm
+++ src/Styles.elm
@@ -1,102 +1,90 @@
 module Styles exposing (..)
 
-import Css exposing (..)
-import Svg.Styled as Svg
-import Html.Styled exposing (Attribute)
-import Html.Styled.Attributes exposing (css)
+import Html exposing (Attribute)
+import Html.Attributes exposing (style)
+import Svg
 
-emojiPicker : Float -> Float -> Attribute msg
+emojiPicker : Float -> Float -> [[Attribute msg]]
 emojiPicker offsetX offsetY =
-    css
-    [ padding (px 0)
-    , backgroundColor (rgb 255 255 255)
-    , borderRadius (px 7)
-    , borderStyle solid
-    , borderWidth (px 1)
-    , borderColor (rgba 0 0 0 0.15)
-    , boxShadow5 (px 0) (px 4) (px 6) (px 0) (rgba 0 0 0 0.15)
-    , boxShadow5 (px 0) (px 6) (px 20) (px 0) (rgba 0 0 0 0.13)
-    , zIndex (int 5)
-    , width (px 315)
-    , height (px 400)
-    , position absolute
-    , top (px offsetY)
-    , left (px offsetX)
-    , textAlign left
+    [ style "padding" "0px"
+    , style "backgroundColor" "rgb(255, 255, 255)"
+    , style "borderRadius" "7px"
+    , style "borderStyle" "solid"
+    , style "borderWidth" "1px"
+    , style "borderColor" "rgba(0, 0, 0, 0.15)"
+    , style "box-shadow" "0px 4px 6px 0px rgba(0, 0, 0, 0.15)"
+    , style "box-shadow" "0px 6px 20px 0px rgba(0, 0, 0, 0.13)"
+    , style "z-index" "5"
+    , style "width" "315px"
+    , style "height" "400px"
+    , style "position" "absolute"
+    , style "top" <| String.fromFloat offsetY ++ "px"
+    , style "left" <| String.fromFloat offsetX ++ "px"
+    , style "text-align" "left"
     ]
     
-emojisMain : Attribute msg
+emojisMain : [Attribute msg]
 emojisMain =
-    css
-    [ width (px 309)
-    , height (px 350)
-    , overflowY auto
-    , padding4 (px 0) (px 0) (px 0) (px 5)
-    , whiteSpace normal
+    [ style "width" "309px"
+    , style "height" "350px"
+    , style "overflow-y" "auto"
+    , style "padding" "0px 0px 0px 5px"
+    , style "white-space" "normal"
     ]
 
-category : Attribute msg
-category =
-    css [ width (px 300) ]
+category : [Attribute msg]
+category = [ style "width" "300px"]
 
-categoryTitle : Attribute msg
+categoryTitle : [Attribute msg]
 categoryTitle =
-    css
-    [ fontSize (px 18)
-    , fontFamily sansSerif
-    , margin4 (px 7) (px 0) (px 0) (px 0)
-    , padding4 (px 2) (px 6) (px 3) (px 7)
-    , color (hex "#555555")
-    , backgroundColor (rgb 255 255 255)
+    [ style "fontSize" "18px"
+    , style "font-family" "sans-serif"
+    , style "margin" "7px 0px 0px 0px"
+    , style "padding" "2px 6px 3px 7px"
+    , style "color" "#555555"
+    , style "background-color" "rgb(255, 255, 255)"
     ]
         
-emoji : Attribute msg
+emoji : [Attribute msg]
 emoji =
-    css
-    [ fontSize (px 27)
-    , fontFamilies ["apple color emoji"]
-    , display inlineBlock
-    , padding4 (px 6) (px 5) (px 3) (px 5)
-    , verticalAlign middle
-    , lineHeight (int 1)
-    , borderRadius (px 5)
-    , hover [ backgroundColor (rgba 0 0 0 0.05)
-            , cursor pointer ]
+    [ style "fontSize" "27px"
+    , style "fontFamilies" "apple color emoji"
+    , style "display" "inline-block"
+    , style "padding" "6px 5px 3px 5px"
+    , style "vertical-align" "middle"
+    , style "line-height" "1"
+    , style "border-radius" "5px"
+    , style "cursor" "pointer"
     ]
 
-iconPanel : Attribute msg
+iconPanel : [Attribute msg]
 iconPanel =
-    css
-    [ width (px 315)
-    , height (px 40)
-    , textAlign center
-    , verticalAlign middle
-    , padding4 (px 7) (px 0) (px 0) (px 0)
-    , borderTopWidth (px 1)
-    , borderTopColor (rgba 0 0 0 0.15)
-    , borderTopStyle solid
+    [ style "width" "315px"
+    , style "height" "40px"
+    , style "text-align" "center"
+    , style "vertical-align" "middle"
+    , style "padding" "7px 0px 0px 0px"
+    , style "border-top-width" "1px"
+    , style "border-top-color" "rgba(0, 0, 0, 0.15)"
+    , style "border-top-style" "solid"
     ]
 
-categoryIcon : Style
-categoryIcon =
-    margin4 (px 5) (px 7) (px 5) (px 6)
+categoryIcon : Svg.Attribute msg
+categoryIcon = Svg.style "margin: 5px 7px 5px 6px"
 
-pathActive : Style
-pathActive =
-    fill (rgba 0 75 200 0.8)
+pathActive : Svg.Attribute msg
+pathActive = Svg.style "fill: rgba(0, 75, 200, 0.8)"
 
-pathInactive : Style
-pathInactive =
-    fill (rgba 0 0 0 0.5)
+pathInactive : Svg.Attribute msg
+pathInactive = Svg.style "fill: rgba(0, 0, 0, 0.5)"
       
-emojiModalBackground : Attribute msg
+emojiModalBackground : [Attribute msg]
 emojiModalBackground =
-    css
-        [ zIndex (int 4)
-        , position fixed
-        , backgroundColor (rgba 0 0 0 0)
-        , width (pct 100)
-        , height (pct 100)
-        , top (px 0)
-        , left (px 0)
-        ]
+    [ style "z-index" "4"
+    , style "position" "fixed"
+    , style "background-color" "white"
+    , style "width" "100%"
+    , style "height" "100%"
+    , style "top" "0px"
+    , style "left" "0px"
+    ]
